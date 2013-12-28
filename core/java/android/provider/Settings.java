ad483c4599f67a441e2d94bc53c486649cf5b07a
MNT <m.zilius@gmail.com>
2013-12-29 00:30:14 +0200

    Network stats: Hide if there's no traffic (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2938,7 +2938,7 @@ public final class Settings {
         */
         public static final String COMBINED_BAR_AUTO_HIDE = "combined_bar_auto_hide";
 
-	/**
+	    /**
          * Whether to show the network status in the status bar
          * @hide
          */
@@ -2948,7 +2948,13 @@ public final class Settings {
          * Frequency at which stats are updated, in milliseconds
          * @hide
          */
-        public static final String STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL = "status_bar_network_stats_update_frequency";   
+        public static final String STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL = "status_bar_network_stats_update_frequency";
+
+        /**
+         * Network stats hide if there's no traffic
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_HIDE = "status_bar_network_hide";
 
         /**
          * Display style of AM/PM next to clock in status bar
