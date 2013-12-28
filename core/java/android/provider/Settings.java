1f6c971c1538edd094d72463cbc5c04fec7510c2
MNT <m.zilius@gmail.com>
2013-12-29 00:13:53 +0200

    Revert "Halo swich"
    This reverts commit 27930884b432b1701191ff37f638690a4e7bc9a5.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2731,12 +2731,6 @@ public final class Settings {
         public static final String POINTER_SPEED = "pointer_speed";
 
         /**
-         * HALO enabled, should default to 0 (no, HALO is disabled)
-         * @hide
-         */
-        public static final String HALO_ENABLED = "halo_enabled";
-
-        /**
          * I am the lolrus.
          * <p>
          * Nonzero values indicate that the user has a bukkit.
